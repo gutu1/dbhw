b81188fb0edfe99ee5cbc8b57c6b3f3f8b436523
gutu1 <55424193+gutu1@users.noreply.github.com>
2021-11-19 13:45:04 +0800

    Add files via upload

---
+++ src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/HwHiAiUser/Desktop/database/hw02/scudb_initial/src
+# Build directory: /home/HwHiAiUser/Desktop/database/hw02/scudb_initial/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
